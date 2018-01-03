850ea35510e53a968f2d2c5038aa9272938300cf
Jake Byers <jebyers3@gmai.com>
2018-01-03 16:32:57 -0700

    adding new intent for messaging tenants

---
+++ app/src/main/java/com/example/owner/adutility_app/sms_n_send.java
@@ -0,0 +1,13 @@
+package com.example.owner.adutility_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sms_n_send extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sms_n_send);
+    }
+}
